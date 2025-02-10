aaa9be2b24cc482fff60a35251f06b67dfe1c9fe
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2025-02-10 03:12:28 -0500

    Update sun.py

--- oop/sun.py
+++ oop/sun.py
@@ -1,6 +1,5 @@
 from celestial_body import CelestialBody
 
-
 class Sun(CelestialBody):
     def __init__(self, name, diameter=None, circumference=None):
         super().__init__(name, diameter, circumference)
