5f9371a944c1b4312a560430d08e890bce26e30b
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2025-02-10 03:11:49 -0500

    Update moon.py

--- oop/moon.py
+++ oop/moon.py
@@ -1,6 +1,4 @@
 from celestial_body import CelestialBody
-
-
 class Moon(CelestialBody):
     def __init__(self, name, diameter=None, circumference=None):
         super().__init__(name, diameter, circumference)
